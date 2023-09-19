96a9a8147596ae88a2b1e6f5f3318f68645380a7
DennySunny <iamdennysunny@gmail.com>
2023-09-19 10:49:24 +0530

    splash screen lottie animations added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DividerComponent } from './divider/divider.component';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 
 
 
 @NgModule({
   declarations: [
-    DividerComponent
+    DividerComponent,
+    SplashScreenComponent
   ],
   imports: [
     CommonModule
